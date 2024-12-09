4bdf695c2c38d758bf1d39c9771e1419af3e3163
Adriel <136254256+Linksyyy@users.noreply.github.com>
2024-12-09 15:09:32 -0300

    Inversão dos if's diminuindo identação

--- main.js
+++ main.js
@@ -50,8 +50,8 @@ function Chess() {
     this.refresh = function (HTMLboard) {
         for (let i = 1; i <= 15; i += 2) {
             for (let j = 1; j <= 15; j += 2) {
-                if (this.board[(i - 1) / 2][(j - 1) / 2] != 0)
-                    HTMLboard.childNodes[j].childNodes[i].innerHTML = this.board[(i - 1) / 2][(j - 1) / 2]
+                if (this.board[(i - 1) / 2][(j - 1) / 2] == 0) break
+                HTMLboard.childNodes[j].childNodes[i].innerHTML = this.board[(i - 1) / 2][(j - 1) / 2]
             }
         }
     }
@@ -67,37 +67,32 @@ function Chess() {
 
     this.takeFirstClick = function () {
         document.addEventListener('click', (e) => {
-            if (typeof e.target.innerText == 'string') {
+            if (typeof e.target.innerText != 'string') return
 
-                this.PieceSelected.column = columns.indexOf(e.target.id[0])
-                this.PieceSelected.row = Number(e.target.id[1]) - 1
-                this.PieceSelected.square = e.target.id
-                this.PieceSelected.piece = e.target.innerText
+            this.PieceSelected.column = columns.indexOf(e.target.id[0])
+            this.PieceSelected.row = Number(e.target.id[1]) - 1
+            this.PieceSelected.square = e.target.id
+            this.PieceSelected.piece = e.target.innerText
 
-                console.log(this.PieceSelected)
+            console.log(this.PieceSelected)
 
-                //document.querySelector(`#${selectedSquare}`)
-            }
+            //document.querySelector(`#${selectedSquare}`)
+
         })
     }
     this.takeSecondClick = function () {
         //incomplete
-        if (this.PieceSelected.have) {
-            if (firstRow + 1 == 2) {
-                console.log('oi')
-                addEventListener('click', (e2) => {
-                    let lastColumn = columns.indexOf(e2.target.id[0])
-                    let lastRow = Number(e2.target.id[1]) - 1
+        if (!this.PieceSelected.have) return
+        console.log('oi')
+        addEventListener('click', (e2) => {
+            let lastColumn = columns.indexOf(e2.target.id[0])
+            let lastRow = Number(e2.target.id[1]) - 1
 
-                    let lastSelectedSquare = e2.target.id
-                    let lastSelectedPiece = e2.target.innerText
+            let lastSelectedSquare = e2.target.id
+            let lastSelectedPiece = e2.target.innerText
 
-                    this.board[lastColumn][lastRow] = PW
-                })
-            }
-
-        }
-
+            this.board[lastColumn][lastRow] = PW
+        })
     }
 }
 
