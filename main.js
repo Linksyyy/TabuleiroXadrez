fdea7d40e7fcdfff985d616cf59797c68ad83dd6
Adriel <136254256+Linksyyy@users.noreply.github.com>
2024-12-10 12:40:04 -0300

    Adicionado seleção das peças com amostra dos possiveis movimentos(incompleto)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-function Chess() {
-    const HTMLboard = document.querySelector('.board')
+function Chess(boardClass) {
+    const HTMLboard = document.querySelector(boardClass)
 
     const selectedSquareColor = 'red'
     //white piecies:
@@ -19,11 +19,11 @@ function Chess() {
     const PB = '♟'
 
     const blackPieces = ['♚', '♛', '♜', '♝', '♞', '♟']
-
+    const whitePieces = ['♔', '♕', '♖', '♗', '♘', '♙']
     const rows = [8, 7, 6, 5, 4, 3, 2, 1]
     const columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
-    function changeSquareColor(square, color) {
+    function changeSquareColor(square, color = selectedSquareColor) {
         document.querySelector(`#${square}`).style.backgroundColor = color
     }
 
@@ -47,7 +47,7 @@ function Chess() {
         [TB, NB, BB, QB, KB, BB, NB, TB],//8
         [PB, PB, PB, PB, PB, PB, PB, PB],//7
         [0, 0, 0, 0, 0, 0, 0, 0],//6
-        [0, 0, 0, 0, 0, 0, 0, 0],//5
+        [0, 0, 0, 0, NW, 0, 0, 0],//5
         [0, 0, 0, 0, 0, 0, 0, 0],//4
         [0, 0, 0, 0, 0, 0, 0, 0],//3
         [PW, PW, PW, PW, PW, PW, PW, PW],//2
@@ -58,7 +58,8 @@ function Chess() {
         for (let i = 1; i <= 15; i += 2) {
             for (let j = 1; j <= 15; j += 2) {
                 HTMLboard.childNodes[j].childNodes[i].innerHTML = this.board[(i - 1) / 2][(j - 1) / 2]
-                if (this.board[(i - 1) / 2][(j - 1) / 2] == 0) HTMLboard.childNodes[j].childNodes[i].innerHTML = ' '
+                if (this.board[(i - 1) / 2][(j - 1) / 2] == 0 || this.board[(i - 1) / 2][(j - 1) / 2] == 1) HTMLboard.childNodes[j].childNodes[i].innerHTML = ''
+                if (this.board[(i - 1) / 2][(j - 1) / 2] == 1) changeSquareColor(HTMLboard.childNodes[j].childNodes[i].id)
             }
         }
     }
@@ -78,17 +79,20 @@ function Chess() {
     this.takeFirstClick = function () {
         document.addEventListener('click', (e) => {
             if (this.PieceSelected.have) return
-
             if (typeof e.target.innerText != 'string') return
+            console.log(1)
+            try {
+                this.PieceSelected.column = columns.indexOf(e.target.id[0])
+                this.PieceSelected.row = rows.indexOf(Number(e.target.id[1]))
+                this.PieceSelected.square = e.target.id
+                this.PieceSelected.piece = e.target.innerText
 
-            this.PieceSelected.column = columns.indexOf(e.target.id[0])
-            this.PieceSelected.row = rows.indexOf(Number(e.target.id[1]))
-            this.PieceSelected.square = e.target.id
-            this.PieceSelected.piece = e.target.innerText
-
-            if (this.PieceSelected.piece == 0) return
-            changeSquareColor(this.PieceSelected.square, selectedSquareColor)
-            this.PieceSelected.have = true
+                if (this.PieceSelected.piece == 0) return
+                this.PieceSelected.have = true
+                changeSquareColor(this.PieceSelected.square)
+                this.pieceMoves()
+                this.refresh()
+            } catch (e) { }
 
         })
 
@@ -96,22 +100,61 @@ function Chess() {
     this.takeSecondClick = function () {
         addEventListener('click', (e) => {
             if (e.target.id == this.PieceSelected.square) return
+            console.log(2)
+            try {
+                this.secondSquareSelected.column = columns.indexOf(e.target.id[0])
+                this.secondSquareSelected.row = rows.indexOf(Number(e.target.id[1]))
+                this.secondSquareSelected.square = e.target.id
+                this.secondSquareSelected.piece = e.target.innerText
 
-            this.secondSquareSelected.column = columns.indexOf(e.target.id[0])
-            this.secondSquareSelected.row = rows.indexOf(Number(e.target.id[1]))
-            this.secondSquareSelected.square = e.target.id
-            this.secondSquareSelected.piece = e.target.innerText
-            
-            //incomplete
-            this.board[this.secondSquareSelected.row][this.secondSquareSelected.column] = this.PieceSelected.piece
-            this.board[this.PieceSelected.row][this.PieceSelected.column] = '0'
-            
-            this.PieceSelected.have = false
+            } catch (e) { this.PieceSelected.have = false }
+
+            //pseude-moviment
+            // this.board[this.secondSquareSelected.row][this.secondSquareSelected.column] = this.PieceSelected.piece
+            // this.board[this.PieceSelected.row][this.PieceSelected.column] = '0'
+
             changeSquareColor(this.PieceSelected.square, '')
-            this.refresh()
+            this.pieceMoves()
+            this.PieceSelected.have = false
         })
+    }
+
+    this.pieceMoves = function () {
+        let possibleMoviments
+        switch (this.PieceSelected.piece) {
+            case '♔':
+                break;
+            case '♕':
+                break;
+            case '♖':
+                break;
+            case '♗':
+                break;
+            case '♘':
+                possibleMoviments = [[2, 1], [2, -1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [1, -2], [-1, -2]]  //linha / coluna
+                break;
+            case '♙':
+                possibleMoviments = [[1, 0], [2, 0]]  //linha / coluna
+                break;
+            default:
+                break;
+        }
+        this.placeColorOnPossibleMoviments(possibleMoviments)
+    }
+
+    this.placeColorOnPossibleMoviments = function (possibleMoviments) {
+
+        let principalColumn = this.PieceSelected.column
+        let principalRow = this.PieceSelected.row
+
+        console.log(this.PieceSelected.column, this.PieceSelected.row)
+
+        for (el of possibleMoviments) {
+            this.board[principalRow - el[0]][principalColumn - el[1]] = 1
+        }
+        this.refresh()
     }
 }
 
-const Xadrez = new Chess()
+const Xadrez = new Chess('.board')
 Xadrez.execute()
